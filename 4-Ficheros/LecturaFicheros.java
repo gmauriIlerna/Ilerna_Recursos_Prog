f52b2fe849fd3d1f4778c807906b9bcd76faefb5
Guilllem <gmauri@ilerna.com>
2025-03-25 14:03:37 +0100

    File and binary file examples added

---
+++ 4-Ficheros/LecturaFicheros.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LecturaFicheros {
+    public static void main(String[] args) {
+        String nombreArchivo = "ejemplo.txt";
+
+        try {
+            FileReader fr = new FileReader(nombreArchivo);
+            BufferedReader br = new BufferedReader(fr);
+
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
